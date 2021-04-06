cdd8e65b618bf99a71361cf4aea626decaa78891
Ted <tedward61194@gmail.com>
2021-04-06 17:32:43 -0400

    Fixed spawning issues for more than one client

--- Assets/Scripts/Players/PaladinAnimationStateController.cs
+++ Assets/Scripts/Players/PaladinAnimationStateController.cs
@@ -4,20 +4,14 @@ using UnityEngine;
 using Mirror;
 
 public class PaladinAnimationStateController : MonoBehaviour {
-    //[SerializeField] GameObject bodyToAnimate;
-
     NetworkPlayer networkPlayer;
     NetworkAnimator networkAnimator;
     Animator animator;
-    //RuntimeAnimatorController runtimeAnimatorController;
 
     public void Start() {
         networkPlayer = GetComponent<NetworkPlayer>();
         networkAnimator = GetComponent<NetworkAnimator>();
         animator = GetComponent<Animator>();
-        //runtimeAnimatorController = GetComponent<RuntimeAnimatorController>();
-        //networkPlayer.GetComponent<Animator>().runtimeAnimatorController = runtimeAnimatorController;
-        
     }
 
     public void SetIsRunning(bool value) {
